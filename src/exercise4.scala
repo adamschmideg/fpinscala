51fb8bc64a1bb858ffc2f475768b0a4118c5b7b9
Adam Schmideg <adam.schmideg@prezi.com>
2014-02-11 22:36:28 +0100

    I used some hint for orElse

--- src/exercise4.scala
+++ src/exercise4.scala
@@ -3,42 +3,21 @@ trait Option[+A] {
     def map[B](f: A => B): Option[B] = {
         this match {
             case Some(a: A) => Some(f(a))
-            case _ => None
+            case None => None
         }
     }
 
-    def getOrElse[B >: A](default: => B): B = {
-        this match {
-            case Some(a: A) => a
-            case _ => default
-        }
-    }
-
-    def _flatMap[B](f: A => Option[B]): Option[B] = {
-        this match {
-            case Some(a: A) => f(a)
-            case _ => None
-        }
+    def getOrElse[B >: A](default: => B): B = this match {
+        case Some(a: A) => a
+        case None => default
     }
 
     def flatMap[B](f: A => Option[B]): Option[B] = {
-        this.map(f).getOrElse(None)
+        map(f) getOrElse None
     }
 
-    def _orElse[B >: A](ob: => Option[B]): Option[B] = {
-        this match {
-            case Some(a: A) => Some(a)
-            case _ => ob
-        }
-    }
-
-    def orElse[B >: A](ob: => Option[B]): Option[B] = ???
-
-    def _filter(f: A => Boolean): Option[A] = {
-        this match {
-            case Some(a: A) if f(a) => Some(a)
-            case _ => None
-        }
+    def orElse[B >: A](ob: => Option[B]): Option[B] = {
+        map(Some(_)) getOrElse ob
     }
 
     def filter(f: A => Boolean): Option[A] = {
@@ -47,6 +26,10 @@ trait Option[+A] {
         else
             None
     }
+
+    def filter_(f: A => Boolean): Option[A] =
+        flatMap(a => if (f(a)) Some(a) else None)
+
 }
 
 
