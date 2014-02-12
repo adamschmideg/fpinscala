17fcf5e35e25061abff91b8ef28a84c625be8355
Adam Schmideg <adam.schmideg@prezi.com>
2014-02-12 16:51:49 +0100

    Well, I looked up the answer there was no hint for exercises 4.1 and 4.2

--- src/exercise4.scala
+++ src/exercise4.scala
@@ -1,35 +1,54 @@
-trait Option[+A] {
+import javax.management.remote.rmi._RMIConnection_Stub
 
-    def map[B](f: A => B): Option[B] = {
+object exercise4 {
+
+// Exercise 4.1
+trait Maybe[+A] {
+
+    def map[B](f: A => B): Maybe[B] = {
         this match {
-            case Some(a: A) => Some(f(a))
-            case None => None
+            case Just(a: A) => Just(f(a))
+            case Nonne => Nonne
         }
     }
 
     def getOrElse[B >: A](default: => B): B = this match {
-        case Some(a: A) => a
-        case None => default
+        case Just(a: A) => a
+        case Nonne => default
     }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = {
-        map(f) getOrElse None
+    def flatMap[B](f: A => Maybe[B]): Maybe[B] = {
+        map(f) getOrElse Nonne
     }
 
-    def orElse[B >: A](ob: => Option[B]): Option[B] = {
-        map(Some(_)) getOrElse ob
+    def orElse[B >: A](ob: => Maybe[B]): Maybe[B] = {
+        map(Just(_)) getOrElse ob
     }
 
-    def filter(f: A => Boolean): Option[A] = {
+    def filter(f: A => Boolean): Maybe[A] = {
         if (this.map(f).getOrElse(false))
             this.map(identity)
         else
-            None
+            Nonne
     }
 
-    def filter_(f: A => Boolean): Option[A] =
-        flatMap(a => if (f(a)) Some(a) else None)
+    def filter_(f: A => Boolean): Maybe[A] =
+        flatMap(a => if (f(a)) Just(a) else Nonne)
 
 }
+case class Just[A](value:A) extends Maybe[A]
+case object Nonne extends Maybe[Nothing]
 
 
+// Exercise 4.2
+def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty)
+        None
+    else
+        Some(xs.sum / xs.length)
+
+def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+}
+
+}
